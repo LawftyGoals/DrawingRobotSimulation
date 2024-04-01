05dbf0e872575ba8fb790419386852301f666b88
Lawfty Goals <mymsnadressis@hotmail.com>
2024-04-01 19:57:39 +0200

    make the tool work closer to the robot

--- DrawingRobotSimulation.js
+++ DrawingRobotSimulation.js
@@ -2,8 +2,10 @@
 
 const canvasHeight = 540;
 const canvasWidth = 720;
+const pulseSize = 0.1;
 
-const targetPoint = { x: Math.floor(canvasWidth / 2), y: Math.floor(canvasHeight / 2) };
+//let targetIndex = 0;
+const targetPoint = [{ x: 150, y: 300 }, { x: 300, y: 300 }, { x: 325, y: 400 }] //{ x: Math.floor(canvasWidth / 2), y: Math.floor(canvasHeight / 2) };
 
 
 function main() {
@@ -22,8 +24,8 @@ function main() {
 }
 
 function draw(canvas, overlay) {
-    let limbPosition = { x: 10, y: 10 };
-    let penDown = false;
+    let limbPosition = { x: 0, y: 0 };
+    let penDown = true;
 
     const ctx = canvas.getContext("2d");
     ctx.lineWidth = 2;
@@ -53,7 +55,7 @@ function draw(canvas, overlay) {
 
         if (penDown) drawTool(limbPosition, ctx);
 
-        if (limbPosition.y === targetPoint.y && limbPosition.x === targetPoint.x) {
+        if ((limbPosition.y === targetPoint[0].y && limbPosition.x === targetPoint[0].x) || limbPosition.x >= canvasWidth || limbPosition.y >= canvasHeight) {
             ctx.stroke();
             ctx.closePath();
             clearInterval(nInterval);
@@ -100,23 +102,60 @@ function moveLimbs(limbPosition) {
 
     limbPosition.x += stepXY.x;
     limbPosition.y += stepXY.y;
+
+    console.log(limbPosition)
 }
 
-const stepSize = 1;
+
 
 function step(limbPosition) {
-    if (limbPosition.x === targetPoint.x && limbPosition.y === targetPoint.y) return { x: 0, y: 0 }
 
-    let m = (targetPoint.y - limbPosition.y) / (targetPoint.x - limbPosition.x);
-    let c = limbPosition.y - limbPosition.x * m;
+    const ratioOfStep = linearMagnitude(limbPosition);
+    console.log(ratioOfStep)
 
-    let stepX = limbPosition.x + stepSize;
-    let stepY = stepX * m + c;
-
-    console.log({ x: stepX - limbPosition.x, y: stepY - limbPosition.y });
-
-    return { x: stepX - limbPosition.x, y: stepY - limbPosition.y };
+    return { x: ratioOfStep.returnX, y: ratioOfStep.returnY };
 }
+
+let maginuted = null;
+
+function linearMagnitude(limbPosition) {
+    const linearX = targetPoint[0].x - limbPosition.x;
+    const linearY = targetPoint[0].y - limbPosition.y;
+
+    if (maginuted === null) {
+        maginuted = linearY / linearX
+    }
+    //const magnitude = linearY / linearX;
+
+    console.log(maginuted);
+
+    const workingX = (Math.pow(pulseSize, 2)) / (1 + Math.pow(maginuted, 2))
+
+    const returnX = Math.sqrt(workingX);
+
+    const returnY = Math.sqrt(Math.pow(pulseSize, 2) - workingX);
+
+    return { returnY, returnX };
+}
+
+//const stepSize = 1;
+
+//function step(limbPosition) {
+//    if (limbPosition.x === targetPoint[targetIndex].x && limbPosition.y === targetPoint[targetIndex].y) {
+//        if (targetIndex < targetPoint.length - 1) targetIndex++;
+//        else return { x: 0, y: 0 };
+//    }
+//
+//    let m = (targetPoint[targetIndex].y - limbPosition.y) / (targetPoint[targetIndex].x - limbPosition.x);
+//    let c = limbPosition.y - limbPosition.x * m;
+//
+//    let stepX = limbPosition.x + stepSize;
+//    let stepY = stepX * m + c;
+//
+//    console.log({ x: stepX - limbPosition.x, y: stepY - limbPosition.y });
+//
+//    return { x: (limbPosition.x < targetPoint[targetIndex].x ? 1 : -1) * (stepSize), y: stepY - limbPosition.y };
+//}
 
 
 
