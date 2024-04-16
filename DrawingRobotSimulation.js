10d5a203a86cf756d355a874941a40e02d750f63
Lawfty Goals <mymsnadressis@hotmail.com>
2024-04-16 21:25:29 +0200

    refactor code, add function to draw square

--- DrawingRobotSimulation.js
+++ DrawingRobotSimulation.js
@@ -1,240 +1,277 @@
-
 const display = {
-    ctx: undefined,
-    otx: undefined,
-    canvasHeight: 540,
-    canvasWidth: 720
-}
+  ctx: undefined,
+  otx: undefined,
+  canvasHeight: 540,
+  canvasWidth: 720,
+};
+
+const machine = {
+  currentPosition: { x: 0, y: 0 },
+  direction: { x: 1, y: 1 },
+};
 
 const animation = {
-    previousTimestamp: undefined,
-    stepSize: 1,
-    pulseInterval: 500,
-    pulseMultiplier: 1,
-    penDown: false,
-    targetPointIndex: 0,
-    running: true,
-    perviousPoint: { x: 0, y: 0 },
-    targetPoints: [{ x: 150, y: 300 }, { x: 300, y: 300 }, { x: 325, y: 400 }, { x: 50, y: 50 }, { x: 75, y: 150 }, { x: 300, y: 300 }, { x: 325, y: 400 }, { x: 400, y: 400 }]
-}
+  previousTimestamp: undefined,
+  stepSize: 1,
+  pulseInterval: 500,
+  pulseMultiplier: 1,
+  penDown: false,
+  targetPointIndex: 0,
+  running: true,
+  perviousPoint: { x: 0, y: 0 },
+  targetPoints: [...makeSquare(200, 50)],
+};
 
-const limbPosition = { x: 0, y: 0, xDirection: 1, yDirection: 1 };
+/* TEST ARRAY:
+,
+    { x: 150, y: 300 },
+    { x: 300, y: 300 },
+    { x: 325, y: 400 },
+    { x: 50, y: 50 },
+    { x: 75, y: 150 },
+    { x: 300, y: 300 },
+    { x: 325, y: 400 },
+    { x: 400, y: 400 },
+    */
 
 const bresenham = {
-    err: 0,
+  err: 0,
 
-    dx: 0,
-    dy: 0,
+  dx: 0,
+  dy: 0,
+};
+
+function makeSquare(position, sideLength) {
+  const furtherByHalf = position + sideLength / 2;
+  const closerByHalf = position - sideLength / 2;
+
+  return [
+    { x: closerByHalf, y: closerByHalf },
+    { x: furtherByHalf, y: closerByHalf },
+    { x: furtherByHalf, y: furtherByHalf },
+    { x: closerByHalf, y: furtherByHalf },
+    { x: closerByHalf, y: closerByHalf },
+  ];
 }
 
 function main() {
+  const canvas = document.getElementById("base");
+  const overlayCanvas = document.getElementById("overlay");
 
-    const canvas = document.getElementById("base");
-    const overlayCanvas = document.getElementById("overlay");
+  canvas.height = display.canvasHeight;
+  canvas.width = display.canvasWidth;
 
-    canvas.height = display.canvasHeight;
-    canvas.width = display.canvasWidth;
+  overlayCanvas.height = display.canvasHeight;
+  overlayCanvas.width = display.canvasWidth;
 
-    overlayCanvas.height = display.canvasHeight;
-    overlayCanvas.width = display.canvasWidth;
+  if (canvas.getContext) {
+    display.ctx = canvas.getContext("2d");
+    display.otx = overlayCanvas.getContext("2d");
 
-    if (canvas.getContext) {
-        display.ctx = canvas.getContext("2d");
-        display.otx = overlayCanvas.getContext("2d");
+    initialDraw();
 
-        initialDraw();
+    //window.requestAnimationFrame((t) => draw(t));
+    //runAlgorithm(animation.targetPoints);
 
-        //window.requestAnimationFrame((t) => draw(t));
-        //runAlgorithm(animation.targetPoints);
-
-        onStartup();
-        window.requestAnimationFrame((t) => mainLoop(t));
-    }
-}
-
-const machine = {
-    currentPosition: { x: 0, y: 0 },
-    direction: { x: 1, y: 1 }
+    onStartup();
+    window.requestAnimationFrame((t) => mainLoop(t));
+  }
 }
 
 function turnStepperX() {
-    machine.currentPosition.x += machine.direction.x;
+  machine.currentPosition.x += machine.direction.x;
 }
 
 function turnStepperY() {
-    machine.currentPosition.y += machine.direction.y;
+  machine.currentPosition.y += machine.direction.y;
 }
 
-
 function onStartup() {
-    setupBresenhamForPoint();
+  setupBresenhamForPoint();
 }
 
 function mainLoop(timeStamp) {
+  if (
+    machine.currentPosition.x ===
+      animation.targetPoints[animation.targetPointIndex].x &&
+    machine.currentPosition.y ===
+      animation.targetPoints[animation.targetPointIndex].y
+  ) {
+    updateParameters();
+  }
 
-    if (machine.currentPosition.x === animation.targetPoints[animation.targetPointIndex].x && machine.currentPosition.y === animation.targetPoints[animation.targetPointIndex].y) {
-        updateParameters();
-    }
+  if (animation.running) {
+    if (timeStamp >= animation.pulseInterval) {
+      const err2 = 2 * bresenham.err;
 
-    if (animation.running) {
+      if (err2 >= bresenham.dy) {
+        bresenham.err = bresenham.err + bresenham.dy;
 
-        if (timeStamp >= animation.pulseInterval) {
-
-            const err2 = 2 * bresenham.err;
-
-            if (err2 >= bresenham.dy) {
-                bresenham.err = bresenham.err + bresenham.dy;
-
-                if (machine.currentPosition.x !== animation.targetPoints[animation.targetPointIndex].x) {
-                    turnStepperX();
-                }
-            }
-            if (err2 <= bresenham.dx) {
-                bresenham.err = bresenham.err + bresenham.dx;
-
-                if (machine.currentPosition.y !== animation.targetPoints[animation.targetPointIndex].y) {
-                    turnStepperY();
-                }
-            }
-            console.log(machine.currentPosition);
-
-            drawInFunction();
-
+        if (
+          machine.currentPosition.x !==
+          animation.targetPoints[animation.targetPointIndex].x
+        ) {
+          turnStepperX();
         }
+      }
+      if (err2 <= bresenham.dx) {
+        bresenham.err = bresenham.err + bresenham.dx;
 
-        window.requestAnimationFrame((t) => mainLoop(t));
+        if (
+          machine.currentPosition.y !==
+          animation.targetPoints[animation.targetPointIndex].y
+        ) {
+          turnStepperY();
+        }
+      }
+      console.log(machine.currentPosition);
+
+      drawInFunction();
     }
 
-
+    window.requestAnimationFrame((t) => mainLoop(t));
+  }
 }
 
 function updateParameters() {
-
-    if (animation.targetPointIndex < animation.targetPoints.length - 1) {
-        animation.targetPointIndex++;
-        setupBresenhamForPoint();
-        changeDirection();
-        if (animation.targetPointIndex > 0 && !animation.penDown) {
-            lowerPen();
-        }
-        console.log(bresenham.dx, bresenham.dy, bresenham.err);
-    } else {
-        animation.running = false;
+  if (animation.targetPointIndex < animation.targetPoints.length - 1) {
+    animation.targetPointIndex++;
+    setupBresenhamForPoint();
+    changeDirection();
+    if (animation.targetPointIndex > 0 && !animation.penDown) {
+      lowerPen();
     }
+    console.log(bresenham.dx, bresenham.dy, bresenham.err);
+  } else {
+    animation.running = false;
+  }
 }
 
 function setupBresenhamForPoint() {
-
-    bresenham.dx = Math.abs(animation.targetPoints[animation.targetPointIndex].x - machine.currentPosition.x);
-    bresenham.dy = -Math.abs(animation.targetPoints[animation.targetPointIndex].y - machine.currentPosition.y);
-    bresenham.err = 0;
-
+  bresenham.dx = Math.abs(
+    animation.targetPoints[animation.targetPointIndex].x -
+      machine.currentPosition.x
+  );
+  bresenham.dy = -Math.abs(
+    animation.targetPoints[animation.targetPointIndex].y -
+      machine.currentPosition.y
+  );
+  bresenham.err = bresenham.dx + bresenham.dy;
 }
 
 function changeDirection() {
-    const xDifference = animation.targetPoints[animation.targetPointIndex].x >= machine.currentPosition.x;
+  const xDifference =
+    animation.targetPoints[animation.targetPointIndex].x >=
+    machine.currentPosition.x;
 
-    if (xDifference && machine.direction.x === -1) {
-        machine.direction.x = machine.direction.x * -1;
-    }
-    if (!xDifference && machine.direction.x === 1) {
-        machine.direction.x = machine.direction.x * -1;
-    }
+  if (xDifference && machine.direction.x === -1) {
+    machine.direction.x = machine.direction.x * -1;
+  }
+  if (!xDifference && machine.direction.x === 1) {
+    machine.direction.x = machine.direction.x * -1;
+  }
 
-    const yDifference = animation.targetPoints[animation.targetPointIndex].y >= machine.currentPosition.y;
+  const yDifference =
+    animation.targetPoints[animation.targetPointIndex].y >=
+    machine.currentPosition.y;
 
-    if (yDifference && machine.direction.y === -1) {
-        machine.direction.y = machine.direction.y * -1;
-    }
-    if (!yDifference && machine.direction.y === 1) {
-        machine.direction.y = machine.direction.y * -1;
-    }
-
+  if (yDifference && machine.direction.y === -1) {
+    machine.direction.y = machine.direction.y * -1;
+  }
+  if (!yDifference && machine.direction.y === 1) {
+    machine.direction.y = machine.direction.y * -1;
+  }
 }
 
 function initialDraw() {
-    display.ctx.lineWidth = 2;
-    display.ctx.lineCap = "round";
-    display.ctx.beginPath();
+  display.ctx.lineWidth = 2;
+  display.ctx.lineCap = "round";
+  display.ctx.beginPath();
 
-    display.otx.fillStyle = "rgb(50 200 0 / 20%)";
-    display.otx.fillRect(...verticalLimb());
-    display.otx.fillRect(...horizontalLimb());
+  display.otx.fillStyle = "rgb(50 200 0 / 20%)";
+  display.otx.fillRect(...verticalLimb());
+  display.otx.fillRect(...horizontalLimb());
 
-    display.otx.fillStyle = "rgb(0 0 0 / 20%)";
-    display.otx.fillRect(limbPosition.x, limbPosition.y, 16, 16);
-
+  display.otx.fillStyle = "rgb(0 0 0 / 20%)";
+  display.otx.fillRect(
+    machine.currentPosition.x,
+    machine.currentPosition.y,
+    16,
+    16
+  );
 }
 
-
-
-
-
-
 function drawInFunction() {
-    clearOtx();
-    drawLimbs();
-    if (animation.penDown) drawTo();
-
+  clearOtx();
+  drawLimbs();
+  if (animation.penDown) drawTo();
 }
 
 function endDraw() {
-    if ((limbPosition.y === animation.targetPoints[0].y && limbPosition.x === animation.targetPoints[0].x) || limbPosition.x >= display.canvasWidth || limbPosition.y >= display.canvasHeight) {
-        display.ctx.stroke();
-        display.ctx.closePath();
-    }
+  if (
+    (machine.currentPosition.y === animation.targetPoints[0].y &&
+      machine.currentPosition.x === animation.targetPoints[0].x) ||
+    machine.currentPosition.x >= display.canvasWidth ||
+    machine.currentPosition.y >= display.canvasHeight
+  ) {
+    display.ctx.stroke();
+    display.ctx.closePath();
+  }
 }
 
 function clearOtx() {
-    display.otx.clearRect(0, 0, display.canvasWidth, display.canvasHeight);
+  display.otx.clearRect(0, 0, display.canvasWidth, display.canvasHeight);
 }
 
-
 function drawLimbs() {
-    display.otx.fillStyle = "rgb(50 200 0 / 20%)";
-    display.otx.fillRect(...verticalLimb(machine.currentPosition.x));
-    display.otx.fillRect(...horizontalLimb(machine.currentPosition.y));
+  display.otx.fillStyle = "rgb(50 200 0 / 20%)";
+  display.otx.fillRect(...verticalLimb(machine.currentPosition.x));
+  display.otx.fillRect(...horizontalLimb(machine.currentPosition.y));
 
-    display.otx.fillStyle = "rgb(0 0 0 / 20%)";
-    display.otx.fillRect(machine.currentPosition.x, machine.currentPosition.y, 16, 16);
-
+  display.otx.fillStyle = "rgb(0 0 0 / 20%)";
+  display.otx.fillRect(
+    machine.currentPosition.x,
+    machine.currentPosition.y,
+    16,
+    16
+  );
 }
 
 function raisePen() {
-    animation.penDown = false;
-    display.otx.beginPath();
+  animation.penDown = false;
+  display.otx.beginPath();
 }
 
 function lowerPen() {
-    animation.penDown = true;
+  animation.penDown = true;
 }
 
 function drawTo() {
-    display.ctx.lineTo(machine.currentPosition.x + 8, machine.currentPosition.y + 8);
-    display.ctx.stroke();
-
+  display.ctx.lineTo(
+    machine.currentPosition.x + 8,
+    machine.currentPosition.y + 8
+  );
+  display.ctx.stroke();
 }
 
 function horizontalLimb() {
-    const y = machine.currentPosition.y;
-    const x = 0;
+  const y = machine.currentPosition.y;
+  const x = 0;
 
-    const width = display.canvasWidth;
-    const height = 16;
+  const width = display.canvasWidth;
+  const height = 16;
 
-    return [x, y, width, height];
-
+  return [x, y, width, height];
 }
 
 function verticalLimb() {
-    const y = 0;
-    const x = machine.currentPosition.x;
+  const y = 0;
+  const x = machine.currentPosition.x;
 
-    const width = 16;
-    const height = display.canvasHeight;
+  const width = 16;
+  const height = display.canvasHeight;
 
-    return [x, y, width, height];
-
+  return [x, y, width, height];
 }
 
 main();
